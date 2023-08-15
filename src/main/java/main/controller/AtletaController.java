a4e956382ffa75f5d16eb770ded8a0317e7219f8
João Flávio <jfpompeo47@gmail.com>
2023-08-15 17:30:37 -0400

    Adicionado pequenas validações de dados

--- src/main/java/main/controller/AtletaController.java
+++ src/main/java/main/controller/AtletaController.java
@@ -1,8 +1,8 @@
 package main.controller;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.ResponseEntity;
@@ -31,40 +31,60 @@ public class AtletaController {
 		repository.save(atleta);
 		return ResponseEntity.ok("Atleta Cadastrado!");
 	}
+	
 	@PostMapping("/deletar/{id}")
 	public ResponseEntity deleteAtleta(@PathVariable Long id) {
+		try {
+			Atleta atleta =  repository.findById(id).get();
+			repository.deleteById(id);
+		} catch ( java.util.NoSuchElementException e) {
+			return ResponseEntity.badRequest().body("Atleta não encontrado!");
+		}
+		Atleta atleta =  repository.findById(id).get();
 		repository.deleteById(id);
 		return ResponseEntity.ok("Atleta Deletado com sucesso!");
 	}
+	
 	@PostMapping("/atualizar/{id}")
 	public ResponseEntity updateAtleta(@PathVariable Long id, @RequestBody AtletaDTO data) {
-		Atleta atleta = repository.findById(id).get();
-		atleta.setEscola(data.escola().toLowerCase());
-		atleta.setIdade(data.idade());
-		atleta.setNome(data.nome().toLowerCase());
-		atleta.setTipoDeVolei(data.tipoDeVolei());
-		
-		repository.save(atleta);
+		try {
+			Atleta atleta = repository.findById(id).get();
+			atleta.setEscola(data.escola().toLowerCase());
+			atleta.setIdade(data.idade());
+			atleta.setNome(data.nome().toLowerCase());
+			atleta.setTipoDeVolei(data.tipoDeVolei());
+			repository.save(atleta);
+		} catch (NoSuchElementException e) {
+			return ResponseEntity.badRequest().body("Atleta não encontrado!");
+		}
 		return ResponseEntity.ok("Dados do atleta atualizados!");
 	}
+	
 	@GetMapping("/listar")
 	public List<Atleta> listarAtleta() {
 		List<Atleta> atletas = repository.findAll();
 		System.out.println(atletas);
 		return atletas;
 	}
+	
 	@GetMapping("/listar/{nome}")
 	public ResponseEntity listByNome(@PathVariable String nome) {
+		if(repository.findByNome(nome.toLowerCase()).isEmpty()) return ResponseEntity.badRequest().body("Atleta não encontrado!");
 		return ResponseEntity.ok(repository.findByNome(nome.toLowerCase()));
 	}
+	
 	@GetMapping("listar/idade/{idade}")
 	public ResponseEntity listByIdade(@PathVariable int idade) {
+		if (repository.findByIdade(idade).isEmpty()) return ResponseEntity.badRequest().body("idade não encontrado!");
 		return ResponseEntity.ok(repository.findByIdade(idade));
 	}
+	
 	@GetMapping("listar/escola/{escola}")
 	public ResponseEntity listByEscola(@PathVariable String escola) {
+		if(repository.findByEscola(escola.toLowerCase()).isEmpty())return ResponseEntity.badRequest().body("escola não encontrado!");
 		return ResponseEntity.ok(repository.findByEscola(escola.toLowerCase()));
 	}
+	
 	@GetMapping("/listar/tipo/{tipoDeVolei}")
 	public ResponseEntity listByTipoDeVolei(@PathVariable TipoDeVolei tipoDeVolei) {
 		return ResponseEntity.ok(repository.findByTipoDeVolei(tipoDeVolei));
